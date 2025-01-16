fbdae46e05d094077c96b7b8d82e0bced4bbec80
JinKyu-Lim <ljk@wgslabs.com>
2025-01-16 16:10:15 +0900

    Add files via upload

---
+++ NetworkComm/MainForm.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace UDPCommunication
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.txtIPAddress = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.txtSendData = new System.Windows.Forms.TextBox();
+            this.txtReceivedData = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtServerPort = new System.Windows.Forms.TextBox();
+            this.btnStartServer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtIPAddress
+            // 
+            this.txtIPAddress.Location = new System.Drawing.Point(12, 12);
+            this.txtIPAddress.Name = "txtIPAddress";
+            this.txtIPAddress.PlaceholderText = "IP 주소 입력";
+            this.txtIPAddress.Size = new System.Drawing.Size(200, 23);
+            this.txtIPAddress.TabIndex = 0;
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(220, 12);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.PlaceholderText = "포트 번호 입력";
+            this.txtPort.Size = new System.Drawing.Size(100, 23);
+            this.txtPort.TabIndex = 1;
+            // 
+            // txtSendData
+            // 
+            this.txtSendData.Location = new System.Drawing.Point(12, 41);
+            this.txtSendData.Name = "txtSendData";
+            this.txtSendData.PlaceholderText = "전송할 데이터 입력";
+            this.txtSendData.Size = new System.Drawing.Size(308, 23);
+            this.txtSendData.TabIndex = 2;
+            // 
+            // txtReceivedData
+            // 
+            this.txtReceivedData.Location = new System.Drawing.Point(12, 99);
+            this.txtReceivedData.Multiline = true;
+            this.txtReceivedData.Name = "txtReceivedData";
+            this.txtReceivedData.ReadOnly = true;
+            this.txtReceivedData.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtReceivedData.Size = new System.Drawing.Size(400, 150);
+            this.txtReceivedData.TabIndex = 3;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(326, 41);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 4;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtServerPort
+            // 
+            this.txtServerPort.Location = new System.Drawing.Point(12, 70);
+            this.txtServerPort.Name = "txtServerPort";
+            this.txtServerPort.PlaceholderText = "서버 포트 입력";
+            this.txtServerPort.Size = new System.Drawing.Size(200, 23);
+            this.txtServerPort.TabIndex = 5;
+            // 
+            // btnStartServer
+            // 
+            this.btnStartServer.Location = new System.Drawing.Point(220, 70);
+            this.btnStartServer.Name = "btnStartServer";
+            this.btnStartServer.Size = new System.Drawing.Size(100, 23);
+            this.btnStartServer.TabIndex = 6;
+            this.btnStartServer.Text = "Start Server";
+            this.btnStartServer.UseVisualStyleBackColor = true;
+            this.btnStartServer.Click += new System.EventHandler(this.btnStartServer_Click);
+            // 
+            // MainForm
+            // 
+            this.ClientSize = new System.Drawing.Size(424, 261);
+            this.Controls.Add(this.btnStartServer);
+            this.Controls.Add(this.txtServerPort);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtReceivedData);
+            this.Controls.Add(this.txtSendData);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtIPAddress);
+            this.Name = "MainForm";
+            this.Text = "UDP Communication";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.TextBox txtIPAddress;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.TextBox txtSendData;
+        private System.Windows.Forms.TextBox txtReceivedData;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtServerPort;
+        private System.Windows.Forms.Button btnStartServer;
+    }
+}
---
+++ NetworkComm/MainForm.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UDPCommunication
+{
+    public partial class MainForm : Form
+    {
+        private UdpClient udpClient;
+        private UdpClient udpServer;
+        private bool isServerRunning = false;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // Initialize UDP Client
+            udpClient = new UdpClient();
+        }
+
+        private async void btnStartServer_Click(object sender, EventArgs e)
+        {
+            if (isServerRunning)
+            {
+                MessageBox.Show("서버가 이미 실행 중입니다.");
+                return;
+            }
+
+            if (!int.TryParse(txtServerPort.Text, out int serverPort))
+            {
+                MessageBox.Show("유효한 서버 포트를 입력하세요.");
+                return;
+            }
+
+            udpServer = new UdpClient(serverPort);
+            isServerRunning = true;
+
+            // 비동기 수신 대기
+            await Task.Run(() => ReceiveDataAsync());
+            MessageBox.Show("서버 시작됨: " + serverPort);
+        }
+
+        private async Task ReceiveDataAsync()
+        {
+            while (isServerRunning)
+            {
+                try
+                {
+                    var receivedResult = await udpServer.ReceiveAsync();
+                    string receivedMessage = Encoding.UTF8.GetString(receivedResult.Buffer);
+
+                    // 수신된 메시지를 출력
+                    Invoke(new Action(() =>
+                    {
+                        txtReceivedData.AppendText($"[{DateTime.Now}] {receivedMessage}\r\n");
+                    }));
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"수신 중 오류 발생: {ex.Message}");
+                }
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string ipAddress = txtIPAddress.Text;
+                string portText = txtPort.Text;
+                string data = txtSendData.Text;
+
+                if (!IPAddress.TryParse(ipAddress, out IPAddress ip))
+                {
+                    MessageBox.Show("유효한 IP 주소를 입력하세요.");
+                    return;
+                }
+
+                if (!int.TryParse(portText, out int port))
+                {
+                    MessageBox.Show("유효한 포트 번호를 입력하세요.");
+                    return;
+                }
+
+                byte[] sendData = Encoding.UTF8.GetBytes(data);
+                udpClient.Send(sendData, sendData.Length, new IPEndPoint(ip, port));
+
+                txtReceivedData.AppendText($"[{DateTime.Now}] Sent: {data}\r\n");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"데이터 전송 중 오류 발생: {ex.Message}");
+            }
+        }
+    }
+}
